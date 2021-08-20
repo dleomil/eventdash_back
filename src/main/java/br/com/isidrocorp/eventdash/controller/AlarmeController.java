13680f6d513569ea0265dd496584dd2ce12ef5e8
dleomil <dleomil@gmail.com>
2021-08-20 14:27:57 -0300

    Atualizando repo

--- src/main/java/br/com/isidrocorp/eventdash/controller/AlarmeController.java
+++ src/main/java/br/com/isidrocorp/eventdash/controller/AlarmeController.java
@@ -2,14 +2,15 @@ package br.com.isidrocorp.eventdash.controller;
 
 //Import de modulos
 import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.isidrocorp.eventdash.dao.AlarmeDAO;
 import br.com.isidrocorp.eventdash.model.Alarme;
 
 @RestController
+@CrossOrigin("*")
 public class AlarmeController {
 	
 	@Autowired
--- src/main/java/br/com/isidrocorp/eventdash/controller/EventoController.java
+++ src/main/java/br/com/isidrocorp/eventdash/controller/EventoController.java
@@ -5,14 +5,15 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.isidrocorp.eventdash.dao.EventoDAO;
 import br.com.isidrocorp.eventdash.model.Evento;
 
 @RestController
+@CrossOrigin("*")
 public class EventoController {
 
 	@Autowired
